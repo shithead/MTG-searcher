8d10873e8a9b458f20a1770bd9d0bb6444d636cb
Pwny Tail <pwnytail@bsd.services>
2023-04-19 13:16:46 +0200

    attach shell.nix

---
+++ shell.nix
@@ -0,0 +1,32 @@
+# shell.nix
+{ pkgs ? import <nixpkgs> {} }:
+with pkgs;
+let
+  my-perl = perl;
+  perl-with-my-packages = my-perl.withPackages (p: with p; [
+    DBI
+    NetSSLeay
+    CryptPBKDF2    
+    Mojolicious
+    DataPrinter
+    TextCSV
+    DBDSQLite
+  ]);
+  
+in
+pkgs.mkShell {
+  name = "perlzone";
+  buildInputs = [
+    gcc
+    pkg-config
+    glibc.dev
+    perl-with-my-packages
+  ];
+  nativeBuildInputs = [
+    makeWrapper
+  ];
+  #PERL5LIB = with perlPackages; makeFullPerlPath [ DBI NetSSLeay CryptPBKDF2 Mojolicious DataPrinter TextCSV DBDSQLite ] ;
+    
+  shellHook = ''
+  '';
+}
